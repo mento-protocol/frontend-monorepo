c0090dd80f9404102b700f8e7f6414dcf6e8ebec
Lex Stefan <contact.alexandru.stefan@gmail.com>
2025-05-27 16:10:19 +0300

    style(fix): used @source inline to fix twailintv4 issues

--- apps/reserve.mento.org/app/layout.tsx
+++ apps/reserve.mento.org/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import "@repo/ui/globals.css";
+
 import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
