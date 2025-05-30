dd545e7a884ee9039596717dc75119148ac9ffe5
Lex Stefan <contact.alexandru.stefan@gmail.com>
2025-05-30 16:14:36 +0300

    style(ui): removed rounded fixed buttons

--- packages/ui/src/components/ui/coin-input.tsx
+++ packages/ui/src/components/ui/coin-input.tsx
@@ -12,7 +12,7 @@ function CoinInput({
       type={type}
       data-slot="input"
       className={cn(
-        "flex h-9 w-full min-w-0 rounded-md bg-transparent text-base outline-none transition-[color,box-shadow]",
+        "flex h-9 w-full min-w-0 rounded-none bg-transparent text-base outline-none transition-[color,box-shadow]",
         "placeholder:text-muted-foreground text-2xl placeholder:text-2xl",
         "selection:bg-primary/20 selection:text-primary-foreground",
         "file:text-foreground file:inline-flex file:h-7 file:bg-transparent file:text-sm file:font-medium",
