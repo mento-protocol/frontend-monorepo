3b5f9083fafe01762e70c9232433ee75dafd7dcc
Lex Stefan <contact.alexandru.stefan@gmail.com>
2025-05-30 13:07:47 +0300

    style(sonner): readded new sonner

--- apps/app.mento.org/app/page.tsx
+++ apps/app.mento.org/app/page.tsx
@@ -11,12 +11,49 @@ import { TokenId } from "./lib/config/tokens";
 import { Button } from "@repo/ui";
 import { ArrowLeft } from "lucide-react";
 
+import { Toaster, toast } from "@repo/ui";
+import { IconCheck } from "@repo/ui";
+
 export default function SwapPage() {
   const showChart = useAtomValue(showChartAtom);
   const [confirmView, setConfirmView] = useAtom(confirmViewAtom);
 
   return (
     <div className="flex h-full w-full flex-wrap items-center justify-center">
+      <Toaster
+        position="top-right"
+        icons={{
+          success: <IconCheck />,
+        }}
+        toastOptions={{
+          classNames: {
+            toast: "toast",
+            title: "title",
+            description: "description",
+            actionButton: "action-button",
+            cancelButton: "cancel-button",
+            closeButton: "close-button",
+          },
+        }}
+        offset={{ top: "80px" }}
+        mobileOffset={{ top: "96px" }}
+      />
+      {/* <Button
+        variant="outline"
+        onClick={() =>
+          toast.success("Swap Successful", {
+            duration: 5000,
+            description: () => (
+              <>
+                You’ve swapped 2,000 CELO for 700 cUSD. <br />{" "}
+                <a href="#">View Transaction on CeloScan</a>
+              </>
+            ),
+          })
+        }
+      >
+        Show Toast
+      </Button> */}
       <div className="mb-6 w-full max-w-xl">
         <div className="bg-card flex flex-col space-y-6 p-6">
           <div className="flex flex-row items-center justify-between gap-6">
