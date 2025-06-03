741d92f36f20a886c79864b0fdfeb8bfccf77828
Lex Stefan <contact.alexandru.stefan@gmail.com>
2025-06-03 13:39:22 +0300

    style(components): fixed toast succes icon

--- packages/ui/src/components/icons/icon_check.tsx
+++ packages/ui/src/components/icons/icon_check.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-const IconCheck: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
+interface IconCheckProps extends React.SVGProps<SVGSVGElement> {
+  fill?: string;
+}
+
+const IconCheck: React.FC<IconCheckProps> = ({ fill = "black", ...props }) => {
   return (
     <svg
       width="24"
@@ -12,7 +16,7 @@ const IconCheck: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
     >
       <path
         d="M11 17H9V15H11V17ZM9 15H7V13H9V15ZM13 15H11V13H13V15ZM7 13H5V11H7V13ZM15 13H13V11H15V13ZM17 11H15V9H17V11ZM19 9H17V7H19V9Z"
-        fill="black"
+        fill={fill}
       />
     </svg>
   );
