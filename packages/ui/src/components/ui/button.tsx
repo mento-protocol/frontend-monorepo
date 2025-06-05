da7da050ab3410ee28419fa40cad270fca3d7eab
Lex Stefan <contact.alexandru.stefan@gmail.com>
2025-06-05 11:01:19 +0300

    style(components): final possible fix for token btn border

--- packages/ui/src/components/ui/button.tsx
+++ packages/ui/src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-[var(--border)] dark:hover:bg-input/50",
+          "border-[var(--border)] shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-[var(--border)] dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-primary shadow-xs hover:bg-secondary-foreground active:bg-[var(--secondary-active)]",
         ghost:
