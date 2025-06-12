836847a750b404b50f91c7def3d5fd57ae95395d
Alex Streza <astre999@gmail.com>
2025-06-12 19:27:55 +0300

    feat: migrate footer component to shared UI package and standardize usage across apps

--- apps/app.mento.org/app/page.tsx
+++ apps/app.mento.org/app/page.tsx
@@ -36,7 +36,7 @@ export default function SwapPage() {
           mobileOffset={{ top: "96px" }}
         />
         <div className="relative mb-6 w-full max-w-[568px]">
-          <div className="top-decorations before:bg-primary after:bg-card after:-top-15 before:absolute before:-left-5 before:-top-5 before:block before:h-5 before:w-5 after:absolute after:left-0 after:block after:h-10 after:w-10"></div>
+          <div className="top-decorations before:bg-primary after:bg-card after:-top-15 hidden before:absolute before:-left-5 before:-top-5 before:block before:h-5 before:w-5 after:absolute after:left-0 after:block after:h-10 after:w-10 md:block"></div>
           <div
             className={cn(
               "bg-card relative z-50 flex flex-col space-y-6 p-6 md:h-[510px]",
@@ -70,7 +70,7 @@ export default function SwapPage() {
                 : "pointer-events-none bg-transparent opacity-0",
             )}
           />
-          <div className="bottom-decorations before:bg-card after:bg-card after:-bottom-15 before:absolute before:-bottom-5 before:-right-5 before:block before:h-5 before:w-5 before:invert after:absolute after:right-0 after:block after:h-10 after:w-10"></div>
+          <div className="bottom-decorations before:bg-card after:bg-card after:-bottom-15 hidden before:absolute before:-bottom-5 before:-right-5 before:block before:h-5 before:w-5 before:invert after:absolute after:right-0 after:block after:h-10 after:w-10 md:block"></div>
         </div>
       </div>
     </>
