9ebe920ad9497c48b0e1420936106b9258732af8
Alex Streza <astre999@gmail.com>
2025-06-30 17:22:15 +0300

    feat: add governance app with locking and proposal management functionality

---
+++ apps/governance.mento.org/app/lib/config/wagmi.config.ts
@@ -0,0 +1,40 @@
+import { Alfajores, Celo } from "@/lib/config/chains";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import {
+  metaMaskWallet,
+  omniWallet,
+  trustWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { createConfig, http } from "wagmi";
+import { valora } from "@/lib/config/valora.wallet";
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended for Celo chains",
+      wallets: [
+        metaMaskWallet,
+        walletConnectWallet,
+        valora,
+        omniWallet,
+        trustWallet,
+      ],
+    },
+  ],
+  {
+    appName: "Mento Governance",
+    projectId:
+      process?.env?.NEXT_PUBLIC_WALLET_CONNECT_ID || "123123123123123123",
+  },
+);
+const transports = {
+  [Celo.id]: http(Celo.rpcUrls.default.http[0]),
+  [Alfajores.id]: http(Alfajores.rpcUrls.default.http[0]),
+};
+
+export const wagmiConfig = createConfig({
+  chains: [Alfajores, Celo],
+  transports,
+  connectors,
+});
