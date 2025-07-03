97d10ebc3f9d344e7978095246826a767bf5fdf5
Andrew718PLTS <andrii.polovets@mentolabs.xyz>
2025-07-03 15:15:06 +0200

    fix: remove test console.log

--- apps/app.mento.org/app/api/sentry-example-api/route.ts
+++ apps/app.mento.org/app/api/sentry-example-api/route.ts
@@ -9,7 +9,6 @@ class SentryExampleAPIError extends Error {
 }
 // A faulty API route to test Sentry's error monitoring
 export function GET() {
-  console.log("test");
   throw new SentryExampleAPIError(
     "This error is raised on the backend called by the example page.",
   );
