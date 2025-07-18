dbd20f6d7e985e8b2e8ca64fb052491b9c4bea41
Lex Stefan <contact.alexandru.stefan@gmail.com>
2025-07-18 14:09:03 +0300

    style(components): gov navigation update

--- apps/governance.mento.org/app/components/nav/header.tsx
+++ apps/governance.mento.org/app/components/nav/header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { ConnectButton } from "../connect-button";
 
 import {
@@ -19,29 +20,35 @@ export function Header() {
   return (
     <header className="relative z-10">
       <div className="flex h-20 flex-row items-center justify-between gap-6">
-        <a
-          href="https://www.mento.org"
-          className="flex items-center"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Mento.org"
-        >
-          <Logo />
-        </a>
+        <div className="flex flex-row items-center justify-start gap-10">
+          <a
+            href="https://www.mento.org"
+            className="flex items-center"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Mento.org"
+          >
+            <Logo />
+          </a>
+          <NavigationMenu className="hidden md:flex">
+            <NavigationMenuList className="gap-6">
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link href="/">Home</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link href="/create-proposal">My Voting Power</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
         <div className="flex flex-row items-center justify-between gap-2 px-4 md:px-6">
           <ConnectButton />
         </div>
       </div>
-      {/* <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu> */}
     </header>
   );
 }
