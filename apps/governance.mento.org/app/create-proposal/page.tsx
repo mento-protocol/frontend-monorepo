02cca410922700ba81f9e9bd3ea93c8acf0db9f0
Andrew718PLTS <andrii.polovets@mentolabs.xyz>
2025-07-25 16:28:41 +0200

    feat: add governance CI smoke test job

--- apps/governance.mento.org/app/create-proposal/page.tsx
+++ apps/governance.mento.org/app/create-proposal/page.tsx
@@ -1,6 +1,7 @@
 import CreateProposalForm from "../components/create-proposal/create-proposal-form";
 
 export default function CreateProposalPage() {
+  console.log("test");
   return (
     <main className="md:px-22 relative w-full px-4 py-8 md:py-16">
       <h1 className="mb-8 w-full text-3xl font-medium md:mb-16 md:text-center md:text-6xl">
