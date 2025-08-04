ea93fa75228534229dce5f0fdfc2d70ab04161f7
Alex Streza <astre999@gmail.com>
2025-08-04 11:16:39 +0300

    refactor: migrate web3 functionality from governance app to shared web3 package

---
+++ packages/web3/src/contracts/governor/use-queue-proposal.ts
@@ -0,0 +1,63 @@
+import { useCallback } from "react";
+import {
+  useWaitForTransactionReceipt,
+  useWriteContract,
+} from "@repo/web3/wagmi";
+import { useContracts } from "@/contracts/use-contracts";
+import { GovernorABI } from "@/abi/Governor";
+import { WriteContractErrorType } from "wagmi/actions";
+import { useQueryClient } from "@tanstack/react-query";
+import { ProposalQueryKey } from "@/contracts/governor/use-proposal";
+
+const useQueueProposal = () => {
+  const queryClient = useQueryClient();
+  const contracts = useContracts();
+  const {
+    writeContract,
+    isPending: isAwaitingUserSignature,
+    data,
+    ...restWrite
+  } = useWriteContract();
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash: data,
+    });
+
+  const queueProposal = useCallback(
+    (
+      proposalId: bigint,
+      onSuccess?: () => void,
+      onError?: (error?: WriteContractErrorType) => void,
+    ) => {
+      writeContract(
+        {
+          address: contracts.MentoGovernor.address,
+          abi: GovernorABI,
+          functionName: "queue",
+          args: [BigInt(proposalId).valueOf()],
+        },
+        {
+          onSuccess: () => {
+            queryClient.invalidateQueries({
+              queryKey: [ProposalQueryKey],
+            });
+            onSuccess?.();
+          },
+          onError,
+        },
+      );
+    },
+    [contracts.MentoGovernor.address, queryClient, writeContract],
+  );
+
+  return {
+    hash: data,
+    queueProposal,
+    isAwaitingUserSignature,
+    isConfirming,
+    isConfirmed,
+    ...restWrite,
+  };
+};
+
+export default useQueueProposal;
---
+++ packages/web3/src/features/governance/use-add-tokens.ts
@@ -0,0 +1,56 @@
+"use client";
+import { useCallback } from "react";
+import * as mento from "@mento-protocol/mento-sdk";
+import { useAccount, useClient, useConfig } from "@repo/web3/wagmi";
+import { Alfajores, Celo } from "@/config/chains";
+import { getConnectorClient } from "wagmi/actions";
+import { watchAsset } from "viem/actions";
+
+export const useAddTokens = () => {
+  const { chainId } = useAccount();
+  const client = useClient();
+  const config = useConfig();
+
+  const addMento = useCallback(async () => {
+    const mentoTokenAddress =
+      mento.addresses[
+        chainId === Celo.chainId ? Celo.chainId : Alfajores.chainId
+      ]?.MentoToken;
+    if (!mentoTokenAddress) throw new Error("Mento token address not found");
+    const connectorClient = await getConnectorClient(config);
+
+    return await watchAsset(connectorClient, {
+      type: "ERC20",
+      options: {
+        address: mentoTokenAddress,
+        symbol: "MENTO",
+        decimals: 18,
+      },
+    });
+  }, [chainId, config]);
+
+  const addVeMento = useCallback(async () => {
+    if (!chainId || !client?.request) return;
+    const veMentoTokenAddress =
+      mento.addresses[
+        chainId === Celo.chainId ? Celo.chainId : Alfajores.chainId
+      ]?.Locking;
+    if (!veMentoTokenAddress)
+      throw new Error("veMento token address not found");
+    const connectorClient = await getConnectorClient(config);
+
+    return await watchAsset(connectorClient, {
+      type: "ERC20",
+      options: {
+        address: veMentoTokenAddress,
+        symbol: "veMENTO",
+        decimals: 18,
+      },
+    });
+  }, [chainId, client?.request, config]);
+
+  return {
+    addMento,
+    addVeMento,
+  };
+};
