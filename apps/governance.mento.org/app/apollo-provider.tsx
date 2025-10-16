63e8ee336d9150abdb492a1c6ff9c96490a2de11
Philip Paetz <philip.paetz@me.com>
2025-10-16 09:16:13 +0200

    chore: remove superfluous experimental apollo package

--- apps/governance.mento.org/app/apollo-provider.tsx
+++ apps/governance.mento.org/app/apollo-provider.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { ApolloNextAppProvider } from "@apollo/experimental-nextjs-app-support";
+import { ApolloNextAppProvider } from "@apollo/client-integration-nextjs";
 import { PropsWithChildren } from "react";
 import { makeClient } from "./graphql/apollo.client";
 
 export const ApolloProvider = ({ children }: PropsWithChildren) => {
   return (
     <ApolloNextAppProvider makeClient={makeClient}>
+      Test
       {children}
     </ApolloNextAppProvider>
   );
