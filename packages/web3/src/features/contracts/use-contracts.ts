69a5e047596a651c33b27eed31868300c64fe951
Andrew718PLTS <andrii.polovets@mentolabs.xyz>
2025-12-12 13:32:23 +0100

    feat: enable sepolia on prod for gov and regenerate lock file

--- packages/web3/src/features/contracts/use-contracts.ts
+++ packages/web3/src/features/contracts/use-contracts.ts
@@ -1,5 +1,4 @@
 import { CeloSepolia, Celo } from "../../config/chains";
-import { IS_PROD } from "../../utils/environment";
 import { MentoChainContracts } from "../../types";
 import { useAccount, useChains } from "wagmi";
 
@@ -7,9 +6,6 @@ export const useContracts = (): MentoChainContracts => {
   const chains = useChains();
   const { isConnected, chainId } = useAccount();
   const isKnownChain = chainId === Celo.id || chainId === CeloSepolia.id;
-
-  // In production, only allow Celo contracts
-  if (IS_PROD) return Celo.contracts as MentoChainContracts;
 
   return isConnected && isKnownChain
     ? (chains.find((chain) => chain.id === chainId)
