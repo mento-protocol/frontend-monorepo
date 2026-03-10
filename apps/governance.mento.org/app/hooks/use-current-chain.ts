940ad9f87beb4f7cb758bcee201f41896352e401
Andrew718PLTS <andrii.polovets@mentolabs.xyz>
2026-03-10 13:58:08 +0100

    test: disable gov smoke test

--- apps/governance.mento.org/app/hooks/use-current-chain.ts
+++ apps/governance.mento.org/app/hooks/use-current-chain.ts
@@ -5,6 +5,7 @@ import { useAccount } from "@repo/web3/wagmi";
 
 export const useCurrentChain = () => {
   const { chainId } = useAccount();
+  console.log("testGovSmokeDisable");
 
   return chainId === CeloSepolia.id ? CeloSepolia : Celo;
 };
