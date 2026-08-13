500465a51b00d8067c390907e3a8070d7944cf18
Philip Paetz <philip.paetz@me.com>
2026-08-13 11:59:01 +0200

    chore(vercel): advance observation canary to event 08

--- apps/app.mento.org/app/layout.tsx
+++ apps/app.mento.org/app/layout.tsx
@@ -51,7 +51,7 @@ export default async function RootLayout({
   return (
     <html
       lang="en"
-      data-mento-observation-canary="523-v1-app-07"
+      data-mento-observation-canary="523-v1-app-08"
       suppressHydrationWarning
     >
       <body
